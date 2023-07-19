590a2c25ba818544412b3911a9097431322f6161
Samuel Simonney <samuel@MacBook-Pro-de-Samuel-2.local>
2023-07-19 10:36:01 +0200

    first commit : add user, register and login

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'last_username' => 'samuel',
+        ]);
+    }
+}
+
