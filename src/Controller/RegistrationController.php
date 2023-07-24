47abe6987d4f0a42d8927e175db3da8dd6cd6ac4
Samuel Simonney <samuel@MacBook-Pro-de-Samuel-2.local>
2023-07-24 11:19:49 +0200

    new changes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('samuel.simonney@gmail.com', 'Default mail'))
+                    ->from(new Address('intranetformation@gmail.com', 'Default mail'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -77,7 +77,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre mail a été vérifié');
 
         return $this->redirectToRoute('app_register');
     }
