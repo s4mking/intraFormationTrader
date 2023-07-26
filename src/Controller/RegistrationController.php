d84f1e16c168867d8180856560dc6616d1c91851
s4mking <samuel;simonney@gmail.com>
2023-07-26 13:57:46 +0200

    test mails endgrid

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,7 +49,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('intranetformation@gmail.com', 'Default mail'))
+                    ->from(new Address('intranetformation@gmail.com', 'Mail d\'inscription '))
                     ->to($user->getEmail())
                     ->subject('Veuillez confirmer votre mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
