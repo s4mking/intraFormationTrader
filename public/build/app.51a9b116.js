e31e2591866b58a61b83b917cb82cd99f6e6e33f
s4mking <samuel;simonney@gmail.com>
2023-08-29 23:06:05 +0200

    new changes

---
+++ public/build/app.51a9b116.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[143],{8144:(e,s,u)=>{u(9755)}},e=>{e.O(0,[832],(()=>{return s=8144,e(e.s=s);var s}));e.O()}]);
---
+++ public/build/login.5529a63f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[535],{9146:()=>{}},s=>{var e;e=9146,s(s.s=e)}]);
